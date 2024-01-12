d73bbe92ebd49f4a3c511699accee12df51e9e4a
B SAIRAM <37287719+SAIRAM1136@users.noreply.github.com>
2024-01-12 12:28:18 +0530

    Add files via upload

---
+++ Day8/Animal&Bird.py
@@ -0,0 +1,7 @@
+from Animal import *
+fly()
+color()
+
+from Bird import *
+fly()
+color()
---
+++ Day8/Operations.py
@@ -0,0 +1,18 @@
+#Approch 1
+# import Calculator
+#
+# Calculator.add(100,200)
+# Calculator.mul(10,20)
+
+# #Approch 2
+# from Calculator import add,mul
+# add(100,200)
+# mul(10,20)
+
+#Approch 3
+from Calculator import *
+add(100,200)
+mul(10,20)
+
+
+
